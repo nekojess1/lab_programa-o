ecf59b51767ed05cd47d6d0b95b28ed1e502e76e
Jéssica Vieira <45262259+nekojess1@users.noreply.github.com>
2019-07-03 18:05:44 -0300

    aeromoça

---
+++ aeromoça.py
@@ -0,0 +1,112 @@
+
+class No:
+    def __init__(self, dado, prox=None, ant=None):
+        self._dado = dado
+        self._prox = prox
+        self._ant = ant
+
+    def getDado(self):
+        return self._dado
+
+    def setDado(self, dado):
+        self._dado = dado
+
+    def getProx(self):
+        return self._prox
+
+    def setProx(self, prox):
+        self._prox = prox
+
+    def getAnt(self):
+        return self._ant
+
+    def setAnt(self, ant):
+        self._ant = ant
+
+class ListaDuplamenteEncadeada:
+
+    def __init__(self, inicio=None):
+        self._inicio = inicio
+
+    def getInicio(self):
+        return self._inicio
+
+    def setInicio(self, inicio):
+        self._inicio = inicio
+
+    def isVazia(self):
+        return self._inicio == None
+
+    def inserirNoInicio(self, dado):
+        novono = No(dado)
+        if not self.isVazia():
+            novono.setProx(self._inicio)
+            self._inicio.setAnt(novono)
+        self._inicio = novono
+
+    """
+    Este método remove o nó cuja chave está associada
+    à menor distância, daí o vetor distância como parâmetro.
+    """
+    def removerOMenor(self, distancia):
+        if not self.isVazia():
+            i = self._inicio
+            menor = float('inf')
+            while i is not None:
+                if distancia[i.getDado()] < menor:
+                    menor = distancia[i.getDado()]
+                    menorNo = i
+                i = i.getProx()
+        if menorNo is not None:
+            if menorNo.getProx() is not None:
+                menorNo.getProx().setAnt(menorNo.getAnt())
+            if menorNo.getAnt() is not None:
+                menorNo.getAnt().setProx(menorNo.getProx())
+            if menorNo is self._inicio:
+                self._inicio = menorNo.getProx()
+            return menorNo.getDado()
+        return -1
+
+    def __str__(self):
+        s = ""
+        i = self._inicio
+        while i is not None:
+            s += str(i.getDado()) + ", "
+            i = i.getProx()
+        return s
+
+# leitura da entrada
+cidades, qtddevoos = list(map(int, input().split()))
+vertices = [[] for i in range(cidades)]
+# construção do grafo
+for i in range(qtddevoos):
+        u, v, p = list(map(int, input().split()))
+        vertices[u-1].append((v-1, p))
+        vertices[v-1].append((u-1, p))
+  
+#inicializando o dijkstra
+def dijkstra(vertices,origem):
+    Q = ListaDuplamenteEncadeada()
+    dist = [float('Inf')]*cidades
+    for u in range(cidades):
+        Q.inserirNoInicio(u)
+    dist[origem] = 0
+    # laço principal
+    while not Q.isVazia():
+        u = Q.removerOMenor(dist)
+        for v, p in vertices[u]:
+            pesoNovoCaminho = dist[u] + p
+            if pesoNovoCaminho < dist[v]:
+                dist[v] = pesoNovoCaminho
+    return dist
+resultado=[0 for i in range(cidades)]
+for k in range(cidades): 
+        d = dijkstra(vertices, k)
+        for i in range(len(resultado)):
+            if resultado[i]<d[i]:
+                resultado[i]=d[i]
+
+print(min(resultado))
+
+
+# ele printa as distâncias da origem até qualquer uma das cidades
